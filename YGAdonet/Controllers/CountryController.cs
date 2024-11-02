a14d8cae97e84a85c18ecba26732281a16160977
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2024-11-02 11:46:39 +0300

    Add project files.

---
+++ YGAdonet/Controllers/CountryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YGAdonet.Models;
+
+namespace YGAdonet.Controllers
+{
+    public class CountryController : Controller
+    {
+        YGAdonetDBEntities db = new YGAdonetDBEntities();
+
+        [HttpGet]
+        public ActionResult AddCountry()
+        {
+            // Görüntü döndürecegim. Ancak bunu yapmadan önce buraya baglı bir görüntü
+            // var mı kontrol edin.
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddCountry(Country input)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Country.Add(input);
+                db.SaveChanges();
+            }
+            else
+            {
+               
+            }
+            return View();
+        }
+    }
+}
